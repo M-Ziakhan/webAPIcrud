1fbe30cb4b7a2067b5fa8b537d94f8ec05c32672
23058 <23058@23058-OTE>
2022-04-20 11:53:50 +0500

    Add project files.

---
+++ WebApi_Crud/Controllers/CrudApiController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi_Crud.Models;
+
+namespace WebApi_Crud.Controllers
+{
+   
+    public class CrudApiController : ApiController
+    {
+        Web_Api_Crud_DBEntities db = new Web_Api_Crud_DBEntities();
+        [HttpGet]
+        public IHttpActionResult GetEmployee()
+        {
+            List<Employee> list = db.Employees.ToList();
+            return Ok(list);
+
+        }
+
+        public IHttpActionResult GetEmployeeByID(int id)
+        {
+            //List<Employee> list = db.Employees.ToList();
+            var emp = db.Employees.Where(x => x.id == id).FirstOrDefault();
+            return Ok(emp);
+
+        }
+        [HttpPut]
+        public IHttpActionResult GetEmployeeByID(Employee e)
+        {
+            db.Entry(e).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return Ok();
+
+        }
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
+        {
+            var emp = db.Employees.Where(x => x.id == id).FirstOrDefault();
+            if(emp !=null)
+            {
+               db.Entry(emp).State = EntityState.Deleted;
+
+            }
+            db.SaveChanges();
+            return Ok();
+
+        }
+        [HttpPost]
+        public IHttpActionResult GetEmployee(Employee em)
+        {
+            db.Employees.Add(em);
+            db.SaveChanges();
+            return Ok();
+
+        }
+    }
+}
---
+++ WebApi_Crud/Controllers/HomeController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using WebApi_Crud.Models;
+
+namespace WebApi_Crud.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        Web_Api_Crud_DBEntities db = new Web_Api_Crud_DBEntities();
+        HttpClient client = new HttpClient();
+        public ActionResult Emp()
+        {
+            //  List<Employee> lst = new List<Employee>();
+            //  client.BaseAddress = new Uri("https://localhost:44366/api/CrudApi");
+            //var response=  client.GetAsync("CrudApi");
+            //  response.Wait();
+            //  var test = response.Result;
+            //  if(test.IsSuccessStatusCode)
+            //  {
+            //      var display = test.Content.ReadAsAsync <List<Employee>> ();
+            //      display.Wait();
+            //      lst = display.Result;
+            //  }
+            // return View(lst);
+
+            List<Employee> list = new List<Employee>();
+            client.BaseAddress = new Uri("https://localhost:44366/api/CrudApi");
+            var res = client.GetAsync("CrudApi");
+            res.Wait();
+            var test = res.Result;
+            if(test.IsSuccessStatusCode)
+            {
+                var dis = test.Content.ReadAsAsync<List<Employee>>();
+                dis.Wait();
+                list = dis.Result;
+            }
+            return View(list);
+
+        }
+        [HttpGet]
+        public ActionResult Creat()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Creat(Employee emp)
+        {
+            client.BaseAddress = new Uri("https://localhost:44366/api/CrudApi");
+            var resp = client.PostAsJsonAsync<Employee>("CrudApi", emp);
+            resp.Wait();
+            var test = resp.Result;
+            if(test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Emp");
+            }
+            return View("Creat");
+        }
+        public ActionResult Details(int id)
+        {
+            Employee e = null;
+            client.BaseAddress = new Uri("https://localhost:44366/api/CrudApi");
+            var resp = client.GetAsync("CrudApi?id="+id);
+            resp.Wait();
+            var test = resp.Result;
+            if(test.IsSuccessStatusCode)
+            {
+                var disp = test.Content.ReadAsAsync<Employee>();
+                 e = disp.Result;
+            }
+
+            return View(e);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Employee e = null;
+            client.BaseAddress = new Uri("https://localhost:44366/api/CrudApi");
+            var resp = client.GetAsync("CrudApi?id=" + id);
+            resp.Wait();
+            var test = resp.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var disp = test.Content.ReadAsAsync<Employee>();
+                e = disp.Result;
+            }
+
+            return View(e);
+        }
+        [HttpPost]
+        public ActionResult Edit(Employee e)
+        {
+            client.BaseAddress = new Uri("https://localhost:44366/api/CrudApi");
+            var res = client.PutAsJsonAsync <Employee>("CrudApi", e);
+            res.Wait();
+           var test= res.Result;
+            if(test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Emp");
+            }
+            return View("Edit");
+        }
+        public ActionResult Delete(int id)
+        {
+            client.BaseAddress = new Uri("https://localhost:44366/api/CrudApi");
+            var res = client.DeleteAsync("CrudApi?id="+id);
+            res.Wait();
+            var test = res.Result;
+            if(test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Emp");
+            }
+            return View("Emp");
+        }
+        public ActionResult email()
+        {
+            return View();
+        }
+        public ActionResult email2()
+        {
+            return View();
+        }
+    }
+}
